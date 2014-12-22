b6a34a2bf4ce9123fa06e7fccedb789dc9c19947
Johnny Parizek <JohnnyParizek@Pentwater-iMac-2012.local>
2014-12-21 21:35:37 -0500

    Initial Commit

---
+++ MadLib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MadLib
+//
+//  Created by Johnny Parizek on 12/21/14.
+//  Copyright (c) 2014 Johnny Parizek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MadLib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MadLib
+//
+//  Created by Johnny Parizek on 12/21/14.
+//  Copyright (c) 2014 Johnny Parizek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
